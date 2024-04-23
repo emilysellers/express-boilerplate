cb3751c1b0f215156c6714cdf2f69d22d7fd731e
Emily Sellers <emilymelindasellers@gmail.com>
2024-04-23 14:03:23 -0700

    Add two get routes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,22 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
 
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+// app.get("/", (req, res) => res.send("Hello Express"));
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  let message = "Hello json";
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
+    res.json({
+      message: message.toUpperCase(),
+    });
+  } else {
+    res.json({
+      message: message,
+    });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
