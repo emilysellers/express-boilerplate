de0892f74a8ae906ed53e71b77c9f970e3def643
Emily Sellers <emilymelindasellers@gmail.com>
2024-04-25 09:21:50 -0700

    Add middleware

--- myApp.js
+++ myApp.js
@@ -2,9 +2,40 @@ require("dotenv").config();
 let express = require("express");
 let app = express();
 
+app.use(function middleWare(req, res, next) {
+  console.log(req.method, req.path, " - ", req.ip);
+  next();
+});
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 // app.get("/", (req, res) => res.send("Hello Express"));
-app.use("/public", express.static(__dirname + "/public"));
+
+// chain a middleware function to add current time
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => res.json({ time: req.time })
+);
+const carrots = {
+  snack: "carrots 🥕",
+  profile: "crunchy",
+  amount: 7,
+  success: "YES",
+};
+app.get("/carrots", (req, res) => {
+  console.log("CARROTS!");
+  //   console.log(res.headers);
+  res.json(carrots);
+});
+// build an echo server
+app.get("/:word/echo", (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
 
 app.get("/json", (req, res) => {
   let message = "Hello json";
